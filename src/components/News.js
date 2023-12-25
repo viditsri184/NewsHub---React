5f3ef227a8c83bf6d2731c28122380b0e003eab6
viditsri184 <viditsrivastava760@gmail.com>
2023-12-25 16:52:53 +0530

    added a top loading bar and refactored the badge

--- src/components/News.js
+++ src/components/News.js
@@ -35,15 +35,19 @@ export class News extends Component {
     }
 
     async updateNews() {
+        this.props.setProgress(10)
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=07611a0861a547ecb8bb42eb2acf85bc&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
+        this.props.setProgress(30)
         let parsedData = await data.json();
+        this.props.setProgress(70)
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false
         });
+        this.props.setProgress(100)
     }
 
     async componentDidMount() {
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,9 @@ export class NewsItem extends Component {
         return (
             <div className='my-3'>
                 <div className="card">
-                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left : '90%', zIndex : '1'}}>{source}</span>
+                    <div className='d-flex position-absolute justify-content-end' style={{right: '0'}}>
+                    <span className="badge rounded-pill bg-danger" style={{left : '90%', zIndex : '1'}}>{source}</span>
+                    </div>
                     <img src={!imageUrl?"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN7c9YNU8-BL7li7aelS0ij4SzIY27-U0tvQ&usqp=CAU":imageUrl} className="card-img-top" alt="..."/>
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
